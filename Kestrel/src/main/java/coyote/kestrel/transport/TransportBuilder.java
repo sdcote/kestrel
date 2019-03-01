a5e401ee4704dd8a7191bfb446cd369bca1f018e
Steve Cote <sdcote@gmail.com>
2019-03-01 07:56:41 -0500

    Building out transport builder structure

--- Kestrel/src/main/java/coyote/kestrel/transport/TransportBuilder.java
+++ Kestrel/src/main/java/coyote/kestrel/transport/TransportBuilder.java
@@ -3,6 +3,7 @@ package coyote.kestrel.transport;
 import coyote.commons.StringUtil;
 import coyote.loader.cfg.Config;
 import coyote.loader.log.Log;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Hashtable;
@@ -12,70 +13,140 @@ import java.util.Map;
  * This builder caches connections to brokers to keep resources under control.
  */
 public class TransportBuilder extends Config {
-    private String brokerURI = null;
-    private URI currentURI = null;
+  private String scheme = null;
+  private String username = null;
+  private String password = null;
+  private int port = 0;
+  private String host = null;
+  private String query = null;
 
-    static final Map<String, Transport> transportMap = new Hashtable<>();
+  static final Map<String, Transport> transportMap = new Hashtable<>();
 
-    public TransportBuilder setURI(String uri) {
-        brokerURI = uri;
-        return this;
+  public TransportBuilder setURI(String uri) throws IllegalArgumentException {
+    try {
+      URI brokerURI = new URI(uri);
+      setURI(brokerURI);
+    } catch (URISyntaxException e) {
+      throw new IllegalArgumentException("The broker URI string is invalid: '" + e.getMessage() + "'");
+    }
+    return this;
+  }
+
+  public TransportBuilder setURI(URI uri) throws IllegalArgumentException {
+
+    return this;
+  }
+
+  public String getURI() {
+    return null;
+  }
+
+
+  public Transport build() throws IllegalArgumentException {
+    Transport retval = null;
+
+    String brokerScheme = getScheme();
+    String brokerUsername = getUsername();
+    String brokerPassword = getPassword();
+    String brokerHost = getHost();
+    int brokerPort = getPort();
+    String brokerQuery = getQuery();
+
+    if (StringUtil.isNotBlank(getScheme())) {
+      if (Transport.AMQP.equalsIgnoreCase(getScheme()) || Transport.AMQPS.equalsIgnoreCase(getScheme())) {
+        // perform AMQP checks
+      } else if (Transport.JMS.equalsIgnoreCase(getScheme())) {
+        // perform JMS checks
+      } else {
+        Log.warn("The broker scheme is not supported: '" + getScheme() + "'");
+      }
+    } else {
+      Log.warn("The broker scheme is blank or empty");
     }
 
-    public String getURI() {
-        return brokerURI;
-    }
+
+    return retval;
+  }
+
+  private String getQuery() {
+    return query;
+  }
+
+  private int getPort() {
+    return port;
+  }
+
+  private String getHost() {
+    return host;
+  }
+
+  private String getPassword() {
+    return password;
+  }
+
+  private String getUsername() {
+    return username;
+  }
+
+  private int validatePort() {
+    return 0;
+  }
+
+  private String validateHost() {
+    return null;
+  }
+
+  private String validatePassword() {
+    return null;
+  }
+
+  private String validateUsername() {
+    return null;
+  }
+
+  private String validateScheme() {
+    return null;
+  }
 
 
-    public Transport build() throws IllegalArgumentException {
-        Transport retval = null;
-        try {
-            URI uri = new URI(brokerURI);
-            if (StringUtil.isNotBlank(uri.getScheme())) {
-                if (Transport.AMQP.equalsIgnoreCase(uri.getScheme())) {
-                    // perform AMQP checks
-                } else if (Transport.JMS.equalsIgnoreCase(uri.getScheme())) {
-                    // perform JMS checks
-                } else {
-                    Log.warn("The broker URI scheme is not supported: '" + brokerURI + "'");
-                }
-            } else {
-                Log.warn("The broker URI scheme is blank: '" + brokerURI + "'");
-            }
-        } catch (URISyntaxException e) {
-            Log.warn("The broker URI is invalid: '" + brokerURI + "'");
-        }
-        return retval;
-    }
+  public TransportBuilder setScheme(String scheme) {
+    return this;
+  }
+
+  public String getScheme() {
+    return scheme;
+  }
+
+  public TransportBuilder setUsername(String username) {
+    return this;
+  }
+
+  public TransportBuilder setPassword(String password) {
+    return this;
+  }
+
+  public TransportBuilder setHost(String hostname) {
+    return this;
+  }
+
+  public TransportBuilder setPort(int port) {
+    return this;
+  }
 
 
-    public TransportBuilder setScheme(String scheme) {
-        return this;
-    }
+  public TransportBuilder setQuery(String hostname) {
+    return this;
+  }
 
-    public TransportBuilder setUsername(String username) {
-        return this;
-    }
 
-    public TransportBuilder setPassword(String password) {
-        return this;
-    }
+  // This may be AMQP specific
+  public TransportBuilder setVirtualHost(String virtualHost) {
+    return this;
+  }
 
-    public TransportBuilder setHost(String hostname) {
-        return this;
-    }
+  // timeout in milliseconds; zero for infinite
+  public TransportBuilder setConnectionTimeout(int timeout) {
+    return this;
+  }
 
-    public TransportBuilder setPort(int port) {
-        return this;
-    }
-
-    // This may be AMQP specific
-    public TransportBuilder setVirtualHost(String virtualHost) {
-        return this;
-    }
-
-    // timeout in milliseconds; zero for infinite
-    public TransportBuilder setConnectionTimeout(int timeout) {
-        return this;
-    }
 }
