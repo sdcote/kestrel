a1946fb62a8096d149b34fed9e161cef316888de
sdcote <sdcote@aep.com>
2019-03-25 17:31:26 -0400

    better manual test

--- ProfileDomain/src/test/java/coyote/profile/ProfileClientTest.java
+++ ProfileDomain/src/test/java/coyote/profile/ProfileClientTest.java
@@ -5,24 +5,34 @@ import coyote.i13n.StatBoard;
 import coyote.kestrel.transport.StatUtil;
 import coyote.kestrel.transport.Transport;
 import coyote.kestrel.transport.TransportBuilder;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProfileClientTest {
 
 
-  @DisplayName("Simple test successful")
-  @Test
+  @DisplayName("Simple performance successful")
+  @Disabled("no running broker to use")
   void testClient() {
     ProfileClient client = new ProfileProxy();
     Transport transport = new TransportBuilder().setURI("amqp://guest:guest@localhost:5672").build();
     transport.open();
     client.setTransport(transport);
     client.initialize();
-    StatBoard stats = client.getStatBoard();
-    client.retrieveProfile("123");
+
+    // enable timing se we can see performance statistics
+    client.getStatBoard().enableTiming(true);
+
+    // run several requests, one after another
+    for ( int x = 0;x<10;x++ ) {
+      Profile profile = client.retrieveProfile("123");
+      //System.out.println(profile);
+    }
+
+    // show the performance statistics
+    System.out.println(StatUtil.dump(client.getStatBoard()));
     transport.close();
   }
-
 
 }
